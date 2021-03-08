22bac84d3600084183acaf1083631b6b93991bd4
Ilmir Karimov <code.for.func@gmail.com>
2021-03-08 19:20:33 +0400

    Revert "Add mutation: CreateFood"
    This reverts commit 2f1a4e48c3d9ca1536945f3355e261a67f3ee63d.

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,10 @@
 module Types
   class MutationType < Types::BaseObject
-    field :create_food, mutation: Mutations::CreateFood
+    # TODO: remove me
+    field :test_field, String, null: false,
+      description: "An example field added by the generator"
+    def test_field
+      "Hello World"
+    end
   end
 end
