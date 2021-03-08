db80cfd049f3e58a7ece98972bb618d40b10d425
Ilmir Karimov <code.for.func@gmail.com>
2021-03-08 06:43:19 +0400

    Alter id sequence for `foods` table
    Food ids uploaded from CSV-file have significant gaps.
    By defult RDMS will try to fill these voids
    This can create unnecessary problems, so ID sequence has been altered:

--- db/migrate/20210307234801_create_foods.rb
+++ db/migrate/20210307234801_create_foods.rb
@@ -1,5 +1,5 @@
 class CreateFoods < ActiveRecord::Migration[6.1]
-  def change
+  def up
     create_table :foods do |t|
       t.string :data_type
       t.string :description
@@ -8,5 +8,14 @@ class CreateFoods < ActiveRecord::Migration[6.1]
 
       t.timestamps
     end
+
+    # Food ids uploaded from CSV-file have significant gaps.
+    # By defult RDMS will try to fill these voids
+    # This can create unnecessary problems, so ID sequence has been altered:
+    execute "ALTER SEQUENCE foods_id_seq MINVALUE 167512 OWNED BY foods.id START WITH 1105904 RESTART 1105904;"
+  end
+
+  def down
+    drop_table :foods
   end
 end
