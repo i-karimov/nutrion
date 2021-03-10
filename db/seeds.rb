5555c3e7418f58633d49ecc568c26bb83bf22482
Ilmir Karimov <code.for.func@gmail.com>
2021-03-11 00:46:10 +0400

    Edit seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,12 @@
 # create admin
 admin = User.new(email: 'admin@mail.com', password: 'yfghbvthj')
 admin.add_role :admin
-admin.saveAdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+admin.save
+
+if Rails.env.development?
+  AdminUser.create!(
+    email: 'admin@example.com', 
+    password: 'password', 
+    password_confirmation: 'password'
+  ) 
+end
