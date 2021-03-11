213df1e5b00824aeaff2a507d1a7331f1feb5fba
Ilmir Karimov <code.for.func@gmail.com>
2021-03-11 15:43:14 +0400

    fixup! fixup! Install globalize, setup db level localization for `Food`, adjust migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  scope '(:locale)', locale: /#{I18n.available_locales.join("|")}/ do # <== add this
+  scope '(:locale)', locale: /#{I18n.available_locales.join("|")}/ do
     devise_for :admin_users, ActiveAdmin::Devise.config
     ActiveAdmin.routes(self)
     devise_for :users
