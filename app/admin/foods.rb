4ce67b268385280e6fb8dcc85d93f567df8635ae
Ilmir Karimov <code.for.func@gmail.com>
2021-03-13 07:53:10 +0400

    Edit active admin resource: Food

--- app/admin/foods.rb
+++ app/admin/foods.rb
@@ -1,17 +1,8 @@
 ActiveAdmin.register Food do
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  permit_params :descripton, :data_type, :wweia_food_category_id, :publication_date
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:data_type, :description, :wweia_food_category_id, :publication_date]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+  permit_params :description, :data_type, :wweia_food_category_id, :publication_date
+
+  controller do
+    before_action { @per_page = 100 }
+  end
   remove_filter :food_nutrients
 end
