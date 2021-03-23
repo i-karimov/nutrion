5731a8eac231522296540962184a36be6451804b
Ilmir Karimov <code.for.func@gmail.com>
2021-03-24 01:06:04 +0400

    Edit active admim resourse: foods

--- app/admin/foods.rb
+++ app/admin/foods.rb
@@ -1,8 +1,29 @@
 ActiveAdmin.register Food do
   permit_params :description, :data_type, :wweia_food_category_id, :publication_date
 
+  index do
+    selectable_column
+    id_column
+    column :description
+    column :data_type
+    columns
+    actions
+  end
+
   controller do
     before_action { @per_page = 100 }
   end
   remove_filter :food_nutrients
+
+  show do
+    attributes_table(:id, :description, :data_type)
+
+    panel 'simple panel' do
+      table_for resource.food_nutrients.order(:data_points) do
+        column(:amount)
+        column(:data_points)
+        column(:min)
+      end
+    end
+  end
 end
